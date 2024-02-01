9c689b2d33b55a25c2baeb896fe3e86adc169438
gazi fayaz <gazifayaz.16694@gmail.com>
2024-02-01 22:19:09 +0600

    topKFrequentElement.js

--- groupAnagram.js
+++ groupAnagram.js
@@ -1,3 +1,13 @@
+/**
+ * Groups the given array of strings into anagrams.
+ * Anagrams are strings made up of the same letters.
+ *
+ * Loops through the array to find all anagrams for each string.
+ * Uses character counts to check if two strings are anagrams.
+ * Stores grouped anagrams in result array.
+ * Uses a Set to avoid duplicate checks.
+ * Returns the array of anagram groups.
+ */
 var groupAnagrams = function (strs) {
   // console.log(strs)
   result = [];
@@ -7,12 +17,12 @@ var groupAnagrams = function (strs) {
       anagramGroups = [strs[i]];
       for (let j = i + 1; j < strs.length; j++) {
         if (strs[i].length == strs[j].length) {
-          let count = new Array(26).fill(0);
+          let count = new Array(26).fill(0); // creating an array of size 26 filled with zeros. each position indicates count of a letter in the alphabet.
           for (let k = 0; k < strs[i].length; k++) {
-            count[strs[i].charCodeAt(k) - "a".charCodeAt(0)]++;
+            count[strs[i].charCodeAt(k) - "a".charCodeAt(0)]++; // comparing current letter code with base letter code which is 'a' to determine the index of the current letter in count array. then incrementing the count of the current letter by 1.
           }
           for (let k = 0; k < strs[j].length; k++) {
-            count[strs[j].charCodeAt(k) - "a".charCodeAt(0)]--;
+            count[strs[j].charCodeAt(k) - "a".charCodeAt(0)]--; // comparing current letter code with base letter code which is 'a' to determine the index of the current letter in count array. then decreasing the count of the current letter by 1.
           }
           if (count.every((x) => x == 0)) {
             anagramGroups.push(strs[j]);
@@ -30,3 +40,4 @@ const strs = args.slice(0, args.length);
 // console.log(strs)
 result = groupAnagrams(strs);
 console.log(result);
+console.log(result.length)
---
+++ topKFrequentElement.js
@@ -0,0 +1,60 @@
+/**
+ * Function to find the k most frequent elements in an array.
+ * @param {number[]} nums - The array of numbers.
+ * @param {number} k - The number of most frequent elements to find.
+ * @returns {number[]} - An array of the k most frequent elements.
+ */
+var topKFrequent = function (nums, k) {
+
+    // An empty object 'count' is created to store each number from the 'nums' array as a key and its frequency as the value.
+    let count = {};
+
+    // A for loop is used to iterate over the 'nums' array.
+    for (let i = 0; i < nums.length; i++) {
+
+        // For each number in the 'nums' array, if the number already exists as a key in the 'count' object, its value (frequency) is incremented by 1. 
+        // If the number does not exist as a key in the 'count' object, it is added with a value (frequency) of 1.
+        count[nums[i]] = (count[nums[i]] || 0) + 1;
+    }
+
+    // An empty array 'result' is created to store the 'k' most frequent numbers.
+    let result = [];
+
+    // A for loop is used to iterate 'k' times.
+    for (let i = 0; i < k; i++) {
+
+        // Variables 'max' and 'max_key' are created and set to 0 and empty string respectively. 
+        // These will be used to keep track of the number with the highest frequency and its corresponding key.
+        let max = 0;
+        let max_key = "";
+
+        // A for-of loop is used to iterate over the entries in the 'count' object.
+        for (const [key, value] of Object.entries(count)){
+
+            // If the frequency of the current key is greater than 'max', 'max' is updated to the current frequency and 'max_key' is updated to the current key.
+            if(value > max){
+                max = value;
+                max_key = key;
+            }
+        }
+
+        // The key with the highest frequency is added to the 'result' array.
+        result.push(max_key);
+
+        // The key that was just added to the 'result' array is removed from the 'count' object to prevent it from being added to the 'result' array again in the next iteration.
+        delete count[max_key];
+    }
+
+    // The 'result' array, which contains the 'k' most frequent numbers, is returned.
+    return result;
+};
+
+const args = process.argv.slice(2);
+const k = args.slice(args.length - 1);
+const nums = args.slice(0, args.length - 1).map((num) => {
+  return parseInt(num);
+});
+console.log(nums);
+console.log(k);
+const result = topKFrequent(nums, k);
+console.log(result);
