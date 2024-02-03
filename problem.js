52eacfb130c1d3bb713689329549c529b458c858
gazi fayaz <gazifayaz.16694@gmail.com>
2024-02-03 12:48:11 +0600

    Add array and hashing folder and move relevant codes

--- problem.js
+++ problem.js
@@ -1,30 +1,30 @@
 function packShirts(numOfBags, numOfShirts) {
-  /*
+    /*
     Each element is considered contents of bags where indices are the bag numbers..there are two possible contents, shirts indicated by numbers or bags indicated with an index of the array
   */
-  let bags = [];
+    let bags = [];
 
-  /*
+    /*
     highest number of bags but is lower than the total bags that can be filled equally with all the shirts
   */
-  let optimalBags; //highest number of bags but is lower than the total bags that can be filled equally with all the shirts
+    let optimalBags; //highest number of bags but is lower than the total bags that can be filled equally with all the shirts
 
-  for (let i = numOfBags; i > 0; i -= 1) {
-    if (numOfShirts % i == 0) {
-      optimalBags = i; //highest value of bags that can be filled equally
+    for (let i = numOfBags; i > 0; i -= 1) {
+        if (numOfShirts % i == 0) {
+            optimalBags = i; //highest value of bags that can be filled equally
 
-      for (let i = 0; i < optimalBags; i++) {
-        bags[i] = numOfShirts / optimalBags; // fill the first optimal number of bags with shirts
-      }
+            for (let i = 0; i < optimalBags; i++) {
+                bags[i] = numOfShirts / optimalBags; // fill the first optimal number of bags with shirts
+            }
 
-      for (let i = optimalBags; i < numOfBags; i++) {
-        bags[i] = `bags[${i - optimalBags}]`; // fill the remaining bags with with filled up bags
-      }
-      break;
+            for (let i = optimalBags; i < numOfBags; i++) {
+                bags[i] = `bags[${i - optimalBags}]`; // fill the remaining bags with with filled up bags
+            }
+            break;
+        }
     }
-  }
-  // console.log(bags)
-  return bags; // return the array of bags
+    // console.log(bags)
+    return bags; // return the array of bags
 }
 // Read inputs from command-line arguments
 const numOfBags = parseInt(process.argv[2]);
